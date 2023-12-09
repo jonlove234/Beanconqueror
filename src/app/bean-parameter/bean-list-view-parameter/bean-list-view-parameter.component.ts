52f91bdf152eaca0611e8a19454bc01704b40be3
Adrian John <adrianjohn6543@gmail.com>
2023-12-09 12:21:41 +0100

    rm: useless injects and empty onInit

--- src/app/bean-parameter/bean-list-view-parameter/bean-list-view-parameter.component.ts
+++ src/app/bean-parameter/bean-list-view-parameter/bean-list-view-parameter.component.ts
@@ -1,9 +1,7 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Settings } from '../../../classes/settings/settings';
 import { UISettingsStorage } from '../../../services/uiSettingsStorage';
-import { UIPreparationStorage } from '../../../services/uiPreparationStorage';
-import { UIAnalytics } from '../../../services/uiAnalytics';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
@@ -11,16 +9,14 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
   templateUrl: './bean-list-view-parameter.component.html',
   styleUrls: ['./bean-list-view-parameter.component.scss'],
 })
-export class BeanListViewParameterComponent implements OnInit {
-  public bean_segment = 'general';
+export class BeanListViewParameterComponent {
+  public bean_segment = 'general' as const;
   public debounceChanges: Subject<string> = new Subject<string>();
   public data: Settings;
-  private numerator: number = 0;
+  private numerator = 0;
 
   constructor(
     public uiSettingsStorage: UISettingsStorage,
-    private readonly uiPreparationStorage: UIPreparationStorage,
-    private readonly uiAnalytics: UIAnalytics,
     private readonly changeDetectorRef: ChangeDetectorRef
   ) {
     this.debounceChanges
@@ -31,9 +27,7 @@ export class BeanListViewParameterComponent implements OnInit {
     this.data = this.uiSettingsStorage.getSettings();
   }
 
-  public ngOnInit() {}
-
-  public triggerChanges(_query): void {
+  public triggerChanges(_query: any): void {
     this.debounceChanges.next(this.numerator.toString());
     this.numerator = this.numerator + 1;
   }
